8aacc0c9f5ce981b432bc4b2c443498b688d5100
hamidie00235 <ahmathisseinh@gmail.com>
2023-08-31 17:31:05 +0300

    Routing is configured

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-    <div className="container">
-     <ListEmployeeComponent  />
-    </div>
-    <FooterComponent />
+      <Router>
+       
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={ListEmployeeComponent} />
+              <Route path="/employees" component={ListEmployeeComponent} />
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
