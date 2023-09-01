101b79195fad860d51401379c984192b9b415476
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-01 12:36:57 +0300

    React add Employee form handling

--- src/components/CreateEmployeeComponent.jsx
+++ src/components/CreateEmployeeComponent.jsx
@@ -1,15 +1,108 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class CreateEmployeeComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        }};
+            firstName: '',
+            lastName: '',
+            emailId: '',
+        };
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.changeEmailHandler = this.changeEmailHandler.bind(this);
+        this.saveEmployee = this.saveEmployee.bind(this);
+    }
+
+    changeFirstNameHandler = (e) => {
+        this.setState({ firstName: e.target.value });
+    };
+
+    changeLastNameHandler = (e) => {
+        this.setState({ lastName: e.target.value });
+    };
+
+    changeEmailHandler = (e) => {
+        this.setState({ emailId: e.target.value });
+    };
+
+    saveEmployee = (e) => {
+        e.preventDefault();
+        let employee = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            emailId: this.state.emailId,
+        };
+        console.log('employee => ' + JSON.stringify(employee));
+
+        EmployeeService.createEmployee(employee).then((res) => {
+            this.props.history.push('/employees');
+        });
+    };
+
+    cancel() {
+        this.props.history.push('/employees');
+    }
+
     render() {
         return (
             <div>
-                <h1>Employee Form</h1>
-                
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3 className="text-center">Add Employee</h3>
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label>First Name: </label>
+                                        <input
+                                            placeholder="First Name"
+                                            name="firstName"
+                                            className="form-control"
+                                            value={this.state.firstName}
+                                            onChange={this.changeFirstNameHandler}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Last Name: </label>
+                                        <input
+                                            placeholder="Last Name"
+                                            name="lastName"
+                                            className="form-control"
+                                            value={this.state.lastName}
+                                            onChange={this.changeLastNameHandler}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Email Address: </label>
+                                        <input
+                                            placeholder="Email Address"
+                                            name="emailId"
+                                            className="form-control"
+                                            value={this.state.emailId}
+                                            onChange={this.changeEmailHandler}
+                                        />
+                                    </div>
+
+                                    <button
+                                        className="btn btn-success"
+                                        onClick={this.saveEmployee}
+                                    >
+                                        Save
+                                    </button>
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={this.cancel.bind(this)}
+                                        style={{ marginLeft: '10px' }}
+                                    >
+                                        Cancel
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
